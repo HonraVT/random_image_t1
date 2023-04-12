fa7098c54ceb3dc4f275537a906c90c1e041541c
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-04-11 21:48:24 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_file
+import random
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    image_dir = "static/images"
+    image_files = os.listdir(image_dir)
+    image_file = random.choice(image_files)
+    return send_file(os.path.join(image_dir, image_file), mimetype="image/jpeg")
+
+if __name__ == "__main__":
+    app.run()
